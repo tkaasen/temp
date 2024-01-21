4b4762cb4fe6063cb68d7ef4609847dfe0bf52f4
tkaasen <torkaa@gmail.com>
2024-01-21 12:32:16 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ fetch('data.json') // Fetch the JSON file
   .then(response => response.json()) // Parse the JSON data
   .then(data => {
     const nameElement = document.getElementById('temp'); // Get the HTML element to display the name
-    nameElement.innerText = data.name; // Display the name from the JSON data
+    nameElement.innerText = data.temp; // Display the name from the JSON data
   })
   .catch(error => console.error('Error fetching data:', error)); // Handle any errors
